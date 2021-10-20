101d65aac395c9f6eeb5678bdb272f6784f5a027
EmilyBLHardy <89961875+EmilyBLHardy@users.noreply.github.com>
2021-10-20 18:39:25 +1100

    Inserted forecast HTML into JS
    Removed forecast HTML from index.HTML
    We added in a loop to run through the days in the array
    Using dummy data
    Proof of concept for now

--- src/script.js
+++ src/script.js
@@ -23,6 +23,33 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                  <div class="weather-forecast-day">
+                    ${day}
+                  </div>
+                  <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" 
+                  width=42px 
+                  />
+                  <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18°</span>
+                  <span class="weather-forecast-temperature-min">12°</span>
+                  </div>
+                </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -99,3 +126,5 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Inverloch");
+
+displayForecast();
